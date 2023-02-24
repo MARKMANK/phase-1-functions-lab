bf5ed2c53ffe8495c3c549a997373ae3f8cddb4b
kevinmarkman <kevin.w.markman@gmail.com>
2023-02-23 19:18:21 -0600

    Adds functions for distance traveled in block, feet, and calcualtes price fare

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+
+const feetInABlock = 264;
+const freeFare = 400;
+
+function distanceFromHqInBlocks(someValue) {
+    if (someValue >= 42){
+        return someValue - 42;
+    } else {
+        return 42 -someValue;
+    }
+  }
+
+  function distanceFromHqInFeet(distanceInBlocks) {
+    const distanceFromHq = distanceFromHqInBlocks(distanceInBlocks)
+    const computedDistanceInFeet = distanceFromHq  * feetInABlock;
+    return computedDistanceInFeet;
+  }
+
+  function distanceTravelledInFeet(start, destination){
+    let positiveDistanceTraveled;
+    if (destination > start){
+        positiveDistanceTraveled = (destination - start);
+    } else {
+        positiveDistanceTraveled = (start - destination);
+    }
+    return positiveDistanceTraveled * feetInABlock
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const feetToPrice = (Math.abs(start-destination) * feetInABlock);
+    if (feetToPrice - freeFare <= 0){
+        return 0;
+    } else if (feetToPrice > 2500){
+        return 'cannot travel that far';
+    } else if (feetToPrice >= 2000){
+        return 25;
+    } else {
+        return (feetToPrice - freeFare) * 0.02;
+    }
+  }
